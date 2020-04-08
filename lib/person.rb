c3976ac15fc74fde7c5226aaa5aec14de727143d
Jonathan T Dean <jonathantomdean@gmail.com>
2020-04-08 22:40:28 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,22 +1,22 @@
 class Person
   
   # Persons Name
-  def name=(name)
-    @name
-  end
-  
   def name
     @name
   end
   
-  
-  # Persons Job
-  def job=(job)
-    @job
+  def name=(name)
+    @name
   end
-  
+
+  # Persons Job
   def job
     @job
   end
+  
+  def job=(job)
+    @job
+  end
+
     
 end
